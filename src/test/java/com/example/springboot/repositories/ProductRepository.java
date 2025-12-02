71f5972be5363f1bc8df2393f47e5f73f4d5e14a
Miguel Lopes <mpmiguel2007@gmail.com>
2025-12-02 20:24:51 -0300

    Criação do Repositório de 'Proodcts'

---
+++ src/test/java/com/example/springboot/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.example.springboot.repositories;
+
+import com.example.springboot.models.Products;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.rmi.server.UID;
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Products, UUID> {
+}
